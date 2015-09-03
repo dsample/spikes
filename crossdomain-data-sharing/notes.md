80719a5f0575a967466d6c0b6b0bb2ea1fbdd1f1
Duncan Sample <duncan.sample@comparethemarket.com>
2015-09-03 10:49:42 +0100

    Added cross domain data sharing from Gist

---
+++ crossdomain-data-sharing/notes.md
@@ -0,0 +1,5 @@
+# Client-side data sharing approaches
+
+## JSONP
+
+JSONP is just a `script` tag which points to a JavaScript file hosted on the resource's domain. As long as the script is designed well (scoped variables).
---
+++ crossdomain-data-sharing/server.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+require 'json'
+
+# set :bind, 'example.com'
+set :port, '80'
+
+get '/data.jsonp' do
+  headers 'Access-Control-Allow-Origin' => 'http://example.com'
+  content_type 'text/javascript'
+
+  data = {
+    text: 'Data from JS'
+  }
+
+  "jsData = #{data.to_json}"
+end
+
+get '/ajax.js' do
+  headers 'Access-Control-Allow-Origin' => 'http://example.com'
+  content_type :json
+  {
+    text: 'Data from AJAX'
+  }.to_json
+end
+
+get '/event.js' do
+  headers 'Access-Control-Allow-Origin' => 'http://example.com'
+  content_type 'text/javascript'
+
+  data = {
+    text: 'Data from a JavaScript event'
+  }
+
+  "function dataViaEvent(){ var event = new CustomEvent('dataLoaded', { detail: #{data.to_json} }); document.dispatchEvent(event); };" +
+  "function dataViaEventFromAJAX(){ $.get('http://example.net/ajax.js', function(data) { var event = new CustomEvent('dataLoaded', { detail: data }); document.dispatchEvent(event); });};"
+end
+
+get '/' do
+  erb :index
+end
